3392dc793d1f0ce933db88e50aeba686322f1348
Valdecir Padovani <valdecirpadovani.jr@gmail.com>
2018-08-31 14:41:31 -0300

    Endpoint /pedidos/{id} disponivel

--- src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -10,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -22,13 +23,13 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
 	private Pedido pedido;
 	
-	public Pagamento() {
-		
+	public Pagamento() {	
 	}
 
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
