d86f183ba11eb4df29e6ac0ec7f5fad992a3723e
Valdecir Padovani <valdecirpadovani.jr@gmail.com>
2018-11-07 13:03:13 -0200

    Implementando toString de Pedido

--- src/main/java/com/nelioalves/cursomc/config/DevConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -17,16 +17,14 @@ public class DevConfig {
 	@Autowired
 	private DBService dbService;
 	
-	@Value("spring.jpa.hibernate.ddl-auto")
+	@Value("${spring.jpa.hibernate.ddl-auto}")
 	private String strategy;
 	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
 		if(!"create".equals(strategy)) {
 			return false;
-		}
-			
+		}	
 		dbService.instantiateTestDatabase();
 		return true;
 	}
