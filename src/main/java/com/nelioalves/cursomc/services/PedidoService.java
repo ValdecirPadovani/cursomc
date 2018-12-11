f4596f6bfe5a0b7b211ec1c84dc7cc534abda1ce
Valdecir Padovani <valdecirpadovani.jr@gmail.com>
2018-12-11 16:13:18 -0200

    Email HTML

--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -66,7 +66,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		emailService.sendOrderConfimationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 }
