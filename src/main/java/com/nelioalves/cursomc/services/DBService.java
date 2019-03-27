ac393ef9de256a79f1693ca1df5a94caecc3dff8
Valdecir Padovani Junior <valdecir.junior@lago.local>
2019-03-27 13:41:14 -0300

    Adicionando senha a cliente

--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -32,6 +33,9 @@ import com.nelioalves.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -120,7 +124,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli = new Cliente(null, "Maria Silva", "cizim.junior@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli = new Cliente(null, "Maria Silva", "marina.silva@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("12345").toString());
 		
 		cli.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
