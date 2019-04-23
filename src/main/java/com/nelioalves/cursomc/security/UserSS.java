2c97b996aab094e15cc99511f0e3008a8b92c45e
Valdecir Padovani Junior <valdecir.junior@lago.local>
2019-04-23 13:59:33 -0300

    Resticao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/nelioalves/cursomc/security/UserSS.java
+++ src/main/java/com/nelioalves/cursomc/security/UserSS.java
@@ -70,4 +70,7 @@ public class UserSS implements UserDetails{
 		return true;
 	}
 
+	public boolean hasRole(Perfil admin) {
+		return getAuthorities().contains(new SimpleGrantedAuthority(admin.getDescricao()));
+	}
 }
