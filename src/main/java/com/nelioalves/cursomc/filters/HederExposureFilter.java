24947ec4c2d3ec7902c8f241fd7bb2b6dd22b3c1
ValdecirPadovani <valdecirpadovani.jr@gmail.com>
2019-07-04 13:13:43 -0300

    Aumentando o tamanho máximo permitido para upload

--- src/main/java/com/nelioalves/cursomc/filters/HederExposureFilter.java
+++ src/main/java/com/nelioalves/cursomc/filters/HederExposureFilter.java
@@ -1,5 +1,32 @@
 package com.nelioalves.cursomc.filters;
 
-public class HederExposureFilter {
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class HederExposureFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+			throws IOException, ServletException {
+		
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("access-control-expose-heders", "location");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {		
+	}
 
 }
